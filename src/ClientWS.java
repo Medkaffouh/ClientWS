49680ff6d37bbb14a5da9ee21112f86e5a0f9c72
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-20 19:50:32 +0100

    consume the web service in the client side

--- src/ClientWS.java
+++ src/ClientWS.java
@@ -1,9 +1,24 @@
 import proxy.BanqueService;
 import proxy.BanqueWS;
+import proxy.Compte;
+
+import java.util.List;
 
 public class ClientWS {
     public static void main(String[] args) {
-        BanqueService proxy=new BanqueWS().getBanqueServicePort();
-        System.out.println(proxy.conversionEuroToDH(800));
+        BanqueService stubWS=new BanqueWS().getBanqueServicePort();
+        System.out.println(stubWS.conversionEuroToDH(800));
+        Compte cp=stubWS.getCompte(2L);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+        List<Compte> cptes=stubWS.listComptes();
+        cptes.forEach(c ->{
+            System.out.println("-----------------");
+            System.out.println(c.getCode());
+            System.out.println(c.getSolde());
+        });
     }
 }
+
+
+//proxy ou stub
